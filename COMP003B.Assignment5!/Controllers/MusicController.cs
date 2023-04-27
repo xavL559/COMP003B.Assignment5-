178c9d9d561bf99880d2810784908b0bc280c19d
Xavier Leftridge <111714709+xavL559@users.noreply.github.com>
2023-04-27 00:46:05 -0700

    Add project files.

---
+++ COMP003B.Assignment5!/Controllers/MusicController.cs
@@ -0,0 +1,61 @@
+﻿using COMP003B.Assignment5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment5.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class MusicController : ControllerBase
+    {
+        List<MusicPeople> musicPeoples = new List<MusicPeople>();
+
+        public MusicController()
+        {
+            musicPeoples.Add(new MusicPeople { Id = 1, Title = "Still D.R.E", Artist = "Dr. DRE", Album = "2001", ReleaseYear = "1999" });
+            musicPeoples.Add(new MusicPeople { Id = 2, Title = "It Was A Good Day", Artist = "Ice Cube", Album = "The Predator", ReleaseYear = "1992" });
+            musicPeoples.Add(new MusicPeople { Id = 3, Title = "in Da Club", Artist = "50 Cent", Album = "Get Rich or Die Tryin'", ReleaseYear = "2003" });
+            musicPeoples.Add(new MusicPeople { Id = 4, Title = "Dear Mama", Artist = "Tupac", Album = "Me Against the World", ReleaseYear = "1995" });
+            musicPeoples.Add(new MusicPeople { Id = 5, Title = "Regulate", Artist = "Warren G", Album = "Above the Rim", ReleaseYear = "1994" });
+        }
+
+        //Get ALl
+        
+        public List<MusicPeople> Get()
+        {
+            return musicPeoples;
+        }
+
+
+        //Get single id
+        
+        public MusicPeople Get(int id)
+        {
+            return musicPeoples.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        //POST
+        
+        public void Post(MusicPeople val)
+        {
+            musicPeoples.Add(val);
+        }
+
+        //PUT
+        
+        public void Put(int id, [FromBody] string music)
+        {
+
+        }
+
+        //DELETE
+        
+        public void Delete(int id)
+        {
+
+        }
+    }
+
+    }
+
+
---
+++ COMP003B.Assignment5!/Models/MusicPeople.cs
@@ -0,0 +1,16 @@
+﻿namespace COMP003B.Assignment5.Models
+{
+    public class MusicPeople
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Album { get; set; }
+
+        public string ReleaseYear { get; set; }
+    }
+}
+
